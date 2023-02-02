9a3b8eac1fd3a2a74d83f5d84250a808d33df5dd
Emanuelle-Oliveira <manutrindadeoliver@gmail.com>
2023-02-02 13:46:18 -0300

    refactoring

--- src/Components/Titulo/index.jsx
+++ src/Components/Titulo/index.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import styled from "styled-components";
 
-const Titulo = ({ children }) => {
+/**
+ * O styled components cria a classe automaticamente
+ * e seta o children conforme foi chamado na classe pai
+ */
+
+const Titulo = styled.h1`
+  color: grey;
+  padding: 25px 0;
+`
+
+/*const Titulo = ({ children }) => {
   return <h1 className="titulo">{children}</h1>;
-};
+};*/
 export default Titulo;
