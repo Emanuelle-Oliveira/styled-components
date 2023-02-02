58d5fd744be4c0607e8de667ef5a6286212f3452
Emanuelle-Oliveira <manutrindadeoliver@gmail.com>
2023-02-02 15:24:16 -0300

    final project

--- src/Components/Conta/index.jsx
+++ src/Components/Conta/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import privado from "../../assets/images/privado.svg";
 import olho_icone from "../../assets/images/olho.svg";
 import dinheiro from "../../assets/images/dinheiro.svg";
-import { Icone, Box, Detalhe, Saldo, Botao } from "../UI";
+import { Icone, Box, Detalhe, Saldo, Botao, IconeTema } from "../UI";
 import styled from "styled-components";
 
 // Herança de estilos
@@ -23,7 +23,7 @@ const Conta = () => {
       <div style={{ fontSize: "26px", padding: "20px 0" }}>
         Saldo disponível{" "}
         <span>
-          <Icone src={dinheiro} alt="Ícone Saldo" />
+          <IconeTema src={dinheiro} alt="Ícone Saldo" />
         </span>
         {toggleState ? (
           <Saldo>
